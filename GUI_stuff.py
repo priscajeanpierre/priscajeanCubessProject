ef3c39f17d381fee199ada359ce6ec73efd88df9
priscajeanpierre <80940705+priscajeanpierre@users.noreply.github.com>
2023-02-26 14:40:57 -0500

    Added GUI file, changed database structure

---
+++ GUI_stuff.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+import sqlite3
+
+import databaseStuff
+
+# Create a new Tkinter window
+
+window = tk.Tk()
+    # Set the window title
+window.title("Wufoo Data")
+
+    # Set the window size
+window.geometry("400x300")
+
+listbox = tk.Listbox(window)
+listbox.pack()
+
+cursor = databaseStuff.connect_database()
+cursor.execute("SELECT entry_id, first_name,last_name, email, website_link, phone_number"
+                   "collab_opportunities, project_topic, collab_year FROM WufooData")
+entries = cursor.fetchall()
+
+for entry in entries:
+    listbox.insert(tk.END, f"{entry[0]} - {entry[1]} ({entry[2]}  {entry[3]} {entry[4]} {entry[5]}"
+                               f"{entry[6]} {entry[7]} {entry[8]} ) ")
+
+
+
+
+
+def show_entry_data():
+    # Get the selected entry ID from the listbox
+    selection = listbox.curselection()
+    if selection:
+        entry_id = entries[selection[0]][0]
+        # Retrieve the complete entry data from the database
+        cursor.execute("SELECT * FROM WufooData WHERE id=?", (entry_id,))
+        complete_entry = cursor().fetchone()
+        # Create a new window to display the complete entry data
+        entry_window = tk.Toplevel(window)
+        entry_window.title("Entry Data")
+        entry_window.geometry("400x300")
+        # Add labels to display the complete entry data
+        tk.Label(entry_window, text=f"ID: {complete_entry[0]}").pack()
+        tk.Label(entry_window, text=f"First Name: {complete_entry[1]}").pack()
+        tk.Label(entry_window, text=f"Last Name: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Email: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Website Link: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Phone Number: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Collab Opportunities: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Project Topic: {complete_entry[2]}").pack()
+        tk.Label(entry_window, text=f"Collab Year: {complete_entry[2]}").pack()
+
+
+# Create a button to show the complete entry data
+button = tk.Button(window, text="Show Entry Data", command=show_entry_data)
+button.pack()
+window.mainloop()
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import databaseStuff
 import json
 import sqlite3
 import sys
@@ -7,94 +8,74 @@ from requests.auth import HTTPBasicAuth
 
 
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-# add comment to test workflow
 
-url = "https://pjean12.wufoo.com/api/v3/forms/project-proposal-submission/entries.json"
-
-def get_data() -> dict:
-
-    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
-
-    if response.status_code != 200:  # if we don't get an ok response we have trouble
-        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
-    sys.exit(-1)
-
-    jsonresponse = response.json()
-    return jsonresponse  # json response will be either a dictionary or a list of dictionaries
-    # each dictionary represents a json object
-
-
-def main():
-    data = get_data()
-    data1 = data['Entries']
-    file_to_save = open("output.txt", 'w')
-    save_data(data1, save_file=file_to_save)
-
-
-def save_data(data_to_save: list, save_file=None):
-    for entry in data_to_save:
-        for key, value in entry.items():
-            print(f"{key}: {value}", file=save_file)
-        # now print the spacer
-        print("+++++++++++++++++++++++++++++++++++++++++++++\n_______________________________________________",
-              file=save_file)
-
-def connect_database():
-    db_connection = None
-    try:
-        db_connection = sqlite3.connect('wufoo_db.db')
-        db_cursor = db_connection.cursor()
-        print('Database connection successful')
-        return db_connection, db_cursor
-    except sqlite3.Error as db_error:
-        print(f'Database Error has occurred: {db_error}')
-
-def close_database(connection: sqlite3.Connection):
-    connection.commit()
-    connection.close()
-    print('\n\nDatabase connection closed.')
-
-def create_db(cursor: sqlite3.Cursor):
-    cursor.execute('''CREATE TABLE IF NOT EXISTS wufoo_db(
-    entry_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    website_link TEXT,
-    phone_number TEXT,
-    collab_opportunities TEXT,
-    project_topic TEXT,
-    collab_year TEXT);
-    ''')
-
-
-def create_initial_entries(cursor: sqlite3.Cursor):
-    entry_id = 1
-    first_name  = 'Michelle'
-    last_name = 'Jean'
-    email = 'mjean@gmail.com'
-    website_link = 'www.example.com'
-    phone_number = '555-555-5555'
-    collab_opportunities = 'Networking Event'
-    project_topic = 'Communications'
-    collab_year = '2023'
-    cursor.execute(f'''INSERT INTO wufoo_db(entry_id,
-    first_name, last_name, email,website_link,phone_number, 
-    collab_opportunities, project_topic, collab_year)VALUES ({entry_id},
-    '{first_name}','{last_name}','{email}','{website_link}',
-    '{phone_number}','{collab_opportunities}','{project_topic}','{collab_year}')''')
+# def connect_database():
+#     db_connection = None
+#     try:
+#         db_connection = sqlite3.connect('wufoo_db.db')
+#         db_cursor = db_connection.cursor()
+#         print('Database connection successful')
+#         return db_connection, db_cursor
+#     except sqlite3.Error as db_error:
+#         print(f'Database Error has occurred: {db_error}')
+#
+# def close_database(connection: sqlite3.Connection):
+#     connection.commit()
+#     connection.close()
+#     print('\n\nDatabase connection closed.')
+#
+# def create_db(cursor: sqlite3.Cursor):
+#     cursor.execute('''CREATE TABLE IF NOT EXISTS wufoo_db(
+#     entry_id INTEGER PRIMARY KEY,
+#     first_name TEXT NOT NULL,
+#     last_name TEXT NOT NULL,
+#     email TEXT NOT NULL,
+#     website_link TEXT,
+#     phone_number TEXT,
+#     collab_opportunities TEXT,
+#     project_topic TEXT,
+#     collab_year TEXT);
+#     ''')
+#
+#
+# def create_initial_entries(cursor: sqlite3.Cursor):
+#     entry_id = 1
+#     first_name  = 'Michelle'
+#     last_name = 'Jean'
+#     email = 'mjean@gmail.com'
+#     website_link = 'www.example.com'
+#     phone_number = '555-555-5555'
+#     collab_opportunities = 'Networking Event'
+#     project_topic = 'Communications'
+#     collab_year = '2023'
+#     cursor.execute(f'''INSERT INTO wufoo_db(entry_id,
+#     first_name, last_name, email,website_link,phone_number,
+#     collab_opportunities, project_topic, collab_year)VALUES ({entry_id},
+#     '{first_name}','{last_name}','{email}','{website_link}',
+#     '{phone_number}','{collab_opportunities}','{project_topic}','{collab_year}')''')
 
 
 def main():
-    conn, cursor = connect_database()
+    conn, cursor = databaseStuff.connect_database()
     print(type(conn))
-    create_db(conn)
-    create_initial_entries(cursor)
-    close_database(conn)
+    databaseStuff.create_db(conn)
+    databaseStuff.create_initial_entries(cursor)
+    databaseStuff.close_database(conn)
+    databaseStuff.create_new_window(conn)
+    databaseStuff.show_entry_data(cursor)
 
 
+db_name = "cubesProject.sqlite"
 
-if __name__ == '__main__':
+
+# def main():
+#     json_response = getData.get_wufoo_data()
+#     entries_list = json_response["Entries"]
+#     conn, cursor = open_db(db_name)
+#     databaseStuff.create_entries_table(cursor)
+#     databaseStuff.add_entries_to_db(cursor, entries_list)
+#     close_db(conn)
+
+
+if __name__ == "__main__":
     main()
