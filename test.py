69507850a42c64ee0fa28686c3965f05a1893151
priscajeanpierre <80940705+priscajeanpierre@users.noreply.github.com>
2023-02-26 15:28:14 -0500

    Updated Automated Testing Unit

--- test.py
+++ test.py
@@ -1,16 +1,39 @@
+import databaseStuff
 import unittest
+import sqlite3
 
 
+# Define the unit test
+class test_database(unittest.TestCase):
+    def test_connection_to_db(self):
+        # Open connection to database
+        conn = sqlite3.connect('wufoo_db.db')
+        cursor = conn.cursor()
 
-class MyTestCase(unittest.TestCase):
-    def test_show_database_entries(self):
-        # Call the function to show the database entries
-        entries = show_database_entries()
+        # Execute a query to retrieve the entries from the database
+        cursor.execute('SELECT * FROM entries')
+        entries = cursor.fetchall()
 
-        # Verify that the correct entries are returned
-        expected_entries = [(1, 'John'), (2, 'Jane')]
-        self.assertEqual(entries, expected_entries)
+        # Close the connection to the database
+        conn.close()
 
+        # Return the entries as a list
+        return entries
+    def test_get_database_entries(self):
+
+        entries = databaseStuff.create_initial_entries()
+        self.assertEqual(len(entries), 2)
+        self.assertEqual(entries[0], (1, 'Entry 1', 10.0))
+        self.assertEqual(entries[1], (2, 'Entry 2', 20.0))
+
+    def test_close_db_connection(self):
+        conn = sqlite3.connect('wufoo_db.db')
+        cursor = conn.cursor()
+        conn.commit()
+        conn.close()
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 
